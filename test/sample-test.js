3138d5991edd7d2627f3f10d52e4c84f53bd67bc
samar19 <47981746+samar19@users.noreply.github.com>
2022-02-10 07:19:25 +0200

    update test file

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,42 +1,71 @@
-describe("NFTMarket", function() {
-  it("Should create and execute market sales", async function() {
-    const Market = await ethers.getContractFactory("NFTMarket")
-    const market = await Market.deploy()
-    await market.deployed()
-    const marketAddress = market.address
+const Market= artifacts.require("Market");
+const NFT = artifacts.require("NFT");
 
-    const NFT = await ethers.getContractFactory("NFT")
-    const nft = await NFT.deploy(marketAddress)
-    await nft.deployed()
-    const nftContractAddress = nft.address
 
-    let listingPrice = await market.getListingPrice()
-    listingPrice = listingPrice.toString()
+contract("Market", function ( accounts ) {
 
-    const auctionPrice = ethers.utils.parseUnits('1', 'ether')
-
-    await nft.createToken("https://www.mytokenlocation.com")
-    await nft.createToken("https://www.mytokenlocation2.com")
+  it("assert true", async function () {
+    await Market.deployed();
+    return assert.isTrue(true);
+  });
   
-    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
-    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
-    
-    const [_, buyerAddress] = await ethers.getSigners()
+  it("admin role", async() => {
+    let NFTMarketIn = await Market.deployed();
+    let marketplaceOwner = await NFTMarketIn.getMarketplaceOwner.call();
+    let ADMIN_ROLE = web3.utils.soliditySha3('ADMIN_ROLE');
+    let isOwnerAnAdmin = await NFTMarketIn.hasRole(ADMIN_ROLE,marketplaceOwner);
+    assert.equal(isOwnerAnAdmin,true,"Owner should be the admin");
+  });
 
-    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
-
-    items = await market.fetchMarketItems()
-    items = await Promise.all(items.map(async i => {
-      const tokenUri = await nft.tokenURI(i.tokenId)
-      let item = {
-        price: i.price.toString(),
-        tokenId: i.price.toString(),
-        seller: i.seller,
-        owner: i.owner,
-        tokenUri
-      }
-      return item
-    }))
-    console.log('items: ', items)
+  it('listing price of 25000000 gwei', async() => {
+    let NFTMarketIn = await Market.deployed();
+    let price = await NFTMarketIn.getListingPrice.call();
+    let listingPrice = web3.utils.toWei(price.toString(),'gwei');
+    let expectedPrice = web3.utils.toWei('25000000','gwei');
+    assert.equal(listingPrice,expectedPrice,'Price should be 25000000 gwei');
+  });
+  
+  describe('Change Price', async () => {
+    it('price should be 30000000 gwei',async() => {
+      let NFTMarketIn = await Market.deployed();
+      await NFTMarketIn.Pause({from:accounts[0]});
+      let changeListingPrice = await NFTMarketIn.changeListingPrice(30000000,{from: accounts[0]});
+      await NFTMarketIn.Unpause({from:accounts[0]});
+      let price = await NFTMarketIn.getListingPrice.call();
+      let listingPrice = web3.utils.toWei(price.toString(),'gwei');
+      let expectedPrice = web3.utils.toWei('30000000','gwei');
+      truffleAssert.eventEmitted(changeListingPrice,'logListingPriceChanged',(event) => {
+        return (event.newListingPrice == 30000000);
+      })
+      assert.equal(listingPrice,expectedPrice,'Price should be 30000000 gwei');
+    })
   })
-})
+
+  describe('validator role', async () => {
+    it('grant validtor role',async () => {
+      let NFTMarketIn = await Market.deployed();
+      let validator = accounts[1];
+      let admin = await NFTMarketIn.getMarketplaceOwner.call();
+      let VALIDATOR_ROLE = web3.utils.soliditySha3('VALIDATOR_ROLE');
+      let grantValidator = await NFTMarketIn.grantValidator(validator,{from: admin});
+      let result = await NFTMarketIn.hasRole(VALIDATOR_ROLE,validator);
+      assert.equal(result,true,`${validator} should granted validator role`)
+      truffleAssert.eventEmitted(grantValidator,'RoleGranted',(event) => {
+        return (event.role == VALIDATOR_ROLE && event.account == validator && event.sender == admin);
+      })
+    })
+
+    it('revoke validtor role',async () => {
+      let NFTMarketIn = await Market.deployed();
+      let validator = accounts[1];
+      let admin = await NFTMarketIn.getMarketplaceOwner.call();
+      let VALIDATOR_ROLE = web3.utils.soliditySha3('VALIDATOR_ROLE');
+      let revokeValidator = await NFTMarketIn.revokeValidator(validator,{from: admin});
+      let result = await NFTMarketIn.hasRole(VALIDATOR_ROLE,validator);
+      assert.equal(result,false,`${validator} should revoked validator role`)
+      truffleAssert.eventEmitted(revokeValidator,'RoleRevoked',(event) => {
+        return (event.role == VALIDATOR_ROLE && event.account == validator && event.sender == admin);
+      })
+    })
+  })
+});
